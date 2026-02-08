89e0d09976c0731086d3a86bfba68686dbb6c0bd
Mateusz Kowalczyk <mateuszmkowalczyk@gmail.com>
2026-02-08 19:40:45 +0100

    Initial version of streaming process auto start/stop

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"time"
 )
 
 const basePath = "/dev/shm"
@@ -14,12 +15,16 @@ const basePath = "/dev/shm"
 var (
 	streamPath   = path.Join(basePath, "stream")
 	playlistPath = path.Join(streamPath, "playlist.m3u8")
+	keepAlive    = make(chan struct{})
 )
 
 func main() {
 	fmt.Println("This is petcam 🐶🐱")
 	fmt.Println("Starting streaming...")
-	go startStream()
+
+	quit := make(chan struct{})
+	go runStream(keepAlive, quit)
+	defer func() { quit <- struct{}{} }()
 
 	http.Handle("/segments/", http.StripPrefix("/segments/", http.FileServer(http.Dir(streamPath))))
 	http.HandleFunc("/", playlist)
@@ -29,11 +34,42 @@ func main() {
 		log.Fatalf("Cannot start server: %v", err)
 	}
 
-	// TODO: stop stream process on exit
-	// TODO: stop stream process after a while of inactivity and restart it on first request
+	// TODO: handle Ctrl+C signal gracefully
 }
 
-func startStream() {
+func runStream(keepAlive <-chan struct{}, quit <-chan struct{}) {
+	done := make(chan error, 1)
+	var streamProcess *os.Process
+	for {
+		select {
+		case <-keepAlive:
+			if streamProcess == nil {
+				streamProcess = startStream(done)
+			}
+		case err := <-done:
+			streamProcess = nil
+			if err != nil {
+				log.Printf("Error running command: %v", err)
+			}
+		case <-time.After(30 * time.Second):
+			if streamProcess != nil {
+				if err := streamProcess.Kill(); err != nil {
+					log.Printf("Error while killing the process: %v", err)
+				}
+				log.Println("Stopping streaming process due to inactivity")
+			}
+		case <-quit:
+			if streamProcess != nil {
+				if err := streamProcess.Kill(); err != nil {
+					log.Printf("Error while killing the process: %v", err)
+				}
+			}
+			return
+		}
+	}
+}
+
+func startStream(done chan<- error) *os.Process {
 	if _, err := os.Stat(basePath); err != nil {
 		log.Fatalf("%s cannot be accessed, but is required for the program to run", basePath)
 	}
@@ -41,8 +77,6 @@ func startStream() {
 	if err := os.MkdirAll(streamPath, 0o755); err != nil {
 		log.Fatalf("Cannot create directory: %s", streamPath)
 	}
-
-	defer func() { _ = os.RemoveAll(streamPath) }()
 
 	cmd := exec.Command(
 		"ffmpeg",
@@ -59,12 +93,24 @@ func startStream() {
 		playlistPath,
 	)
 
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("Cannot run streaming proces: %v", err)
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("Cannot start streaming proces: %v", err)
 	}
+
+	go func() {
+		done <- cmd.Wait()
+		if err := os.RemoveAll(streamPath); err != nil {
+			log.Printf("Couldn't remove stream directory: %v", err)
+		}
+	}()
+
+	return cmd.Process
 }
 
 func playlist(w http.ResponseWriter, r *http.Request) {
+	keepAlive <- struct{}{}
+	// TODO: wait for process to start to prevent from 404 on first request
+
 	if _, err := os.Stat(playlistPath); err != nil {
 		log.Printf("Stream not accessible: %v", err)
 		http.NotFound(w, r)
